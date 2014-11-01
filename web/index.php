143ab00600cd8bff61ca190938a6950aee2f95ba
Angelica Chen <angelicachen@Angelicas-MacBook-Pro.local>
2014-11-01 03:26:21 -0400

    cover template

--- web/index.php
+++ web/index.php
@@ -20,3 +20,47 @@ $app->get('/', function() use($app) {
 $app->run();
 
 ?>
+
+<!DOCTYPE html>
+<?php include_once('head.php'); ?>
+<body>
+
+    <div class="site-wrapper">
+
+      <div class="site-wrapper-inner">
+
+        <div class="cover-container">
+
+          <div class="masthead clearfix">
+            <div class="inner">
+              <h3 class="masthead-brand">Cover</h3>
+              <ul class="nav masthead-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#">Features</a></li>
+                <li><a href="#">Contact</a></li>
+              </ul>
+            </div>
+          </div>
+
+          <div class="inner cover">
+            <h1 class="cover-heading">YHack Project!</h1>
+            <p class="lead">Description. Something will go here.</p>
+            <p class="lead">
+              <a href="#" class="btn btn-lg btn-default">Learn more</a>
+            </p>
+          </div>
+
+          <div class="mastfoot">
+            <div class="inner">
+              <p>Developed by Angelica Chen and Anna Matlin, Princeton University</p>
+            </div>
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+    <?php include_once('foot.php'); ?>
+  </body>
+</html>
